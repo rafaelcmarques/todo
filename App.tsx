2bcb6f7e30e4fa2824db8ce22a90f97def5f6164
Rafael Marques <rafaeldecarvalhomarques@gmail.com>
2023-08-16 15:56:39 -0300

    adicionado componente EmptyList

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/theme';
 import { Home } from './src/screen/Home';
 
+
+import {useFonts, Inter_400Regular, Inter_700Bold} from '@expo-google-fonts/inter'
+import { Loading } from './src/Components/Loading';
+
+
 export default function App() {
+  const [fontsLoaded] = useFonts({Inter_400Regular, Inter_700Bold});
   return (
     <ThemeProvider theme={theme}>
       <StatusBar
@@ -12,7 +17,7 @@ export default function App() {
         backgroundColor={'transparent'}
         translucent
       />
-     <Home/>
+      {fontsLoaded ? <Home/> : <Loading/> }
     </ThemeProvider>
   )
 }
